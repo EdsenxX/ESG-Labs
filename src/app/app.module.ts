d860ad7c54acf9d0a28ae823f92bf654b6701531
Eduardo Serrano Jaime <eduardoserranojaime@gmail.com>
2018-11-06 21:36:10 -0600

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,39 @@
+import { RouterModule, Routes } from '@angular/router';
+
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { FooterComponent } from './footer/footer.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { OurworkComponent } from './ourwork/ourwork.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { ContactusComponent } from './contactus/contactus.component';
+
+const routes: Routes = [
+  { path: '', component: InicioComponent},
+  { path: 'ourwork', component: OurworkComponent},
+  { path: 'aboutus', component: AboutusComponent},
+  { path: 'contactus', component: ContactusComponent},
+  { path: '**', component: InicioComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    FooterComponent,
+    InicioComponent,
+    OurworkComponent,
+    AboutusComponent,
+    ContactusComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
